7e6536132542bc1f445f27d97633bf9276d54aed
Timothy Miller <innerlogic4321@gmail.com>
2022-12-05 17:47:47 +0900

    Initial Commit

--- src/Drastic.LNTouchVisualizer/ApiDefinition.cs
+++ src/Drastic.LNTouchVisualizer/ApiDefinition.cs
@@ -1,70 +1,83 @@
+using System;
+using CoreGraphics;
+using Foundation;
+using ObjCRuntime;
+using UIKit;
+
 namespace Drastic.LNTouchVisualizer
 {
 
-// The first step to creating a binding is to add your native framework ("MyLibrary.xcframework")
-// to the project.
-// Open your binding csproj and add a section like this
-// <ItemGroup>
-//   <NativeReference Include="MyLibrary.xcframework">
-//     <Kind>Framework</Kind>
-//     <Frameworks></Frameworks>
-//   </NativeReference>
-// </ItemGroup>
-//
-// Once you've added it, you will need to customize it for your specific library:
-//  - Change the Include to the correct path/name of your library
-//  - Change Kind to Static (.a) or Framework (.framework/.xcframework) based upon the library kind and extension.
-//    - Dynamic (.dylib) is a third option but rarely if ever valid, and only on macOS and Mac Catalyst
-//  - If your library depends on other frameworks, add them inside <Frameworks></Frameworks>
-// Example:
-// <NativeReference Include="libs\MyTestFramework.xcframework">
-//   <Kind>Framework</Kind>
-//   <Frameworks>CoreLocation ModelIO</Frameworks>
-// </NativeReference>
-// 
-// Once you've done that, you're ready to move on to binding the API...
-//
-// Here is where you'd define your API definition for the native Objective-C library.
-//
-// For example, to bind the following Objective-C class:
-//
-//     @interface Widget : NSObject {
-//     }
-//
-// The C# binding would look like this:
-//
-//     [BaseType (typeof (NSObject))]
-//     interface Widget {
-//     }
-//
-// To bind Objective-C properties, such as:
-//
-//     @property (nonatomic, readwrite, assign) CGPoint center;
-//
-// You would add a property definition in the C# interface like so:
-//
-//     [Export ("center")]
-//     CGPoint Center { get; set; }
-//
-// To bind an Objective-C method, such as:
-//
-//     -(void) doSomething:(NSObject *)object atIndex:(NSInteger)index;
-//
-// You would add a method definition to the C# interface like so:
-//
-//     [Export ("doSomething:atIndex:")]
-//     void DoSomething (NSObject object, nint index);
-//
-// Objective-C "constructors" such as:
-//
-//     -(id)initWithElmo:(ElmoMuppet *)elmo;
-//
-// Can be bound as:
-//
-//     [Export ("initWithElmo:")]
-//     NativeHandle Constructor (ElmoMuppet elmo);
-//
-// For more information, see https://aka.ms/ios-binding
-//
+    // @interface LNTouchConfig : NSObject
+    [BaseType(typeof(NSObject))]
+    interface LNTouchConfig
+    {
+        // @property (nonatomic) CGFloat alpha;
+        [Export("alpha")]
+        nfloat Alpha { get; set; }
+
+        // @property (nonatomic) NSTimeInterval fadeDuration;
+        [Export("fadeDuration")]
+        double FadeDuration { get; set; }
+
+        // @property (nonatomic, strong) UIColor * _Nullable strokeColor;
+        [NullAllowed, Export("strokeColor", ArgumentSemantic.Strong)]
+        UIColor StrokeColor { get; set; }
+
+        // @property (nonatomic, strong) UIColor * _Nullable fillColor;
+        [NullAllowed, Export("fillColor", ArgumentSemantic.Strong)]
+        UIColor FillColor { get; set; }
+
+        // @property (readonly, nonatomic, strong, class) LNTouchConfig * _Nonnull rippleConfig;
+        [Static]
+        [Export("rippleConfig", ArgumentSemantic.Strong)]
+        LNTouchConfig RippleConfig { get; }
+
+        // @property (readonly, nonatomic, strong, class) LNTouchConfig * _Nonnull touchConfig;
+        [Static]
+        [Export("touchConfig", ArgumentSemantic.Strong)]
+        LNTouchConfig TouchConfig { get; }
+    }
+
+    // @interface LNTouchVisualizerWindow : UIWindow
+    [BaseType(typeof(UIWindow))]
+    interface LNTouchVisualizerWindow
+    {
+        // @property (getter = isTouchVisualizationEnabled, nonatomic) BOOL touchVisualizationEnabled;
+        [Export("touchVisualizationEnabled")]
+        bool TouchVisualizationEnabled { [Bind("isTouchVisualizationEnabled")] get; set; }
+
+        // @property (getter = isMorphEnabled, nonatomic) BOOL morphEnabled;
+        [Export("morphEnabled")]
+        bool MorphEnabled { [Bind("isMorphEnabled")] get; set; }
+
+        // @property (nonatomic, strong) LNTouchConfig * _Nonnull touchContactConfig;
+        [Export("touchContactConfig", ArgumentSemantic.Strong)]
+        LNTouchConfig TouchContactConfig { get; set; }
+
+        // @property (nonatomic, strong) LNTouchConfig * _Nonnull touchRippleConfig;
+        [Export("touchRippleConfig", ArgumentSemantic.Strong)]
+        LNTouchConfig TouchRippleConfig { get; set; }
+
+        // -(instancetype _Nonnull)initWithFrame:(CGRect)frame touchVisualizationEnabled:(BOOL)touchVisualizationEnabled morphEnabled:(BOOL)morphEnabled contactConfig:(LNTouchConfig * _Nullable)contactConfig rippleConfig:(LNTouchConfig * _Nullable)rippleConfig __attribute__((objc_designated_initializer));
+        [Export("initWithFrame:touchVisualizationEnabled:morphEnabled:contactConfig:rippleConfig:")]
+        [DesignatedInitializer]
+        NativeHandle Constructor(CGRect frame, bool touchVisualizationEnabled, bool morphEnabled, [NullAllowed] LNTouchConfig contactConfig, [NullAllowed] LNTouchConfig rippleConfig);
+    }
+
+#if IOS
+    // @interface TouchVisualizer (UIWindowScene)
+    [Category]
+    [BaseType(typeof(UIWindowScene))]
+    interface UIWindowScene_TouchVisualizer
+    {
+        // @property (nonatomic) BOOL touchVisualizerEnabled;
+        [Export("touchVisualizerEnabled")]
+        bool TouchVisualizerEnabled { get; set; }
+
+        // @property (readonly, nonatomic, strong) LNTouchVisualizerWindow * _Nonnull touchVisualizerWindow;
+        [Export("touchVisualizerWindow", ArgumentSemantic.Strong)]
+        LNTouchVisualizerWindow TouchVisualizerWindow { get; }
+    }
+#endif
 
 }
